fb841dc5c6e692c90b597e4a30a1c787e738b8c7
Matt Lajoie <mlajoie03@gmail.com>
2018-01-31 20:08:21 -0500

    added career component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {AppComponent} from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
+import { CareerComponent } from './career/career.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { HeaderComponent } from './header/header.component';
         AppComponent,
         NavbarComponent,
         AboutComponent,
-        HeaderComponent
+        HeaderComponent,
+        CareerComponent
     ],
     imports: [
         BrowserModule,
